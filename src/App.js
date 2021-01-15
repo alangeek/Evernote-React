40cd64ba85ae5a8ec1469fa31b57996c1473ee00
Alan Christian <alanchristiancreator@hotmail.com>
2021-01-15 00:21:06 -0300

    Project structure, Bulma and Global project style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
-
-const App = () => <p>React Notes</p>
-
+import React, { Fragment } from 'react';
+import "./App.scss";
+import { Notification, Section } from 'rbx';
+ 
+const App = () => (
+  <Fragment>
+    <Section>
+      <Notification color="success">
+        React Notes!
+      </Notification>
+    </Section>
+  </Fragment>
+)
+ 
 export default App;
