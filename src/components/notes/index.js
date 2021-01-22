c25c5ecb2e780376ec38d6038a0ac0c874c1b7d2
Alan Christian <alanchristiancreator@hotmail.com>
2021-01-22 00:09:09 -0300

    Listing as notes

--- src/components/notes/index.js
+++ src/components/notes/index.js
@@ -1,9 +1,35 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import '../../styles/notes.scss';
 import { push as Menu } from 'react-burger-menu';
 import { Column, Button } from "rbx";
+import List from "../notes/list";
+import NotesService from '../../services/notes';
 
 const Notes = (props) => {
+  const [notes, setNotes] = useState([]);
+  const [current_note, setCurrentNote] = useState({ title: "", body: "", id: "" });
+
+  async function fetchNotes() {
+    const response = await NotesService.index();
+    if (response.data.length >= 1) {
+      setNotes(response.data.reverse())
+      setCurrentNote(response.data[0])
+    }
+  }
+
+  const selectNote = (id) => {
+    const note = notes.find((note) => {
+      return note._id == id;
+    })
+    setCurrentNote(note);
+  }
+
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+
   return(
     <Fragment>
      <Column.Group className="notes" id="notes">
@@ -18,10 +44,13 @@ const Notes = (props) => {
         >
           <Column.Group>
             <Column size={10} offset={1}>
-              Search...
+            Pesquisar ...
             </Column>
           </Column.Group>
-          <p>List...</p>
+          <List
+            notes={notes}
+            selectNote={selectNote}
+            current_note={current_note} />
         </Menu>
 
 
